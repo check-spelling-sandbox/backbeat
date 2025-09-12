6baf71499e990fbd5deadb86489a5215fa093da2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-09-12 09:33:21 +0300

    spelling: partitions
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/unit/backbeatConsumer.js
+++ tests/unit/backbeatConsumer.js
@@ -64,7 +64,7 @@ describe('backbeatConsumer', () => {
                 assert(consumer._consumer.pause.notCalled);
             });
 
-            it('should not call pause when no paritions assigned', () => {
+            it('should not call pause when no partitions assigned', () => {
                 consumer._consumer.isConnected = () => true;
                 consumer._consumer.subscription = () => ['example-topic'];
                 consumer._consumer.assignments = () => [];
@@ -100,7 +100,7 @@ describe('backbeatConsumer', () => {
                 assert(consumer._consumer.resume.notCalled);
             });
 
-            it('should not call resume when no paritions are paused', () => {
+            it('should not call resume when no partitions are paused', () => {
                 consumer._consumer.isConnected = () => true;
                 consumer._consumer.subscription = () => ['example-topic'];
                 consumer._consumer.assignments = () => [];
