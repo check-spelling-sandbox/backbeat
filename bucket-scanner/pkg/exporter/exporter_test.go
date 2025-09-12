bdd7a1d8a43bba34e8383bacb1337d4622ed06c6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-09-12 17:28:01 +0300

    spelling: six possible statuses multiplied by two
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- bucket-scanner/pkg/exporter/exporter_test.go
+++ bucket-scanner/pkg/exporter/exporter_test.go
@@ -129,7 +129,7 @@ var _ = Describe("Prometheus Exporter", func() {
 		It("should not leak between per-backend counts", func(done Done) {
 			defer close(done)
 
-			expectMetrics := 12 // 6 possible statuses times 2 destinations
+			expectMetrics := 12 // six possible statuses multiplied by two destinations
 
 			setValues(cs1, cs1.GetTargetLocation("loc1"))
 			setValues(cs2, cs2.GetTargetLocation("loc2"))
